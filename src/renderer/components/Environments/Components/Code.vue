2ec4e3356a095b1054e58548c1e443b35539aab0
Lawrence Turton <lawrence.turton@gmail.com>
2019-10-19 21:33:34 +0100

    added changes

---
+++ src/renderer/components/Environments/Components/Code.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Code'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/renderer/components/Global/Console.vue
@@ -0,0 +1,35 @@
+<template>
+    <section>
+        <header>
+            <nav>
+                <button>PROBLEMS</button>
+                <button>OUTPUT</button>
+                <button>DEBUG CONSOLE</button>
+                <button>TERMINAL</button>
+            </nav>
+            <div class="right">
+                <select>
+                    <option value="">1: Blank Canvas Feathers</option>
+                    <option value="">2: Terminal</option>
+                    <option value="">3: Terminal</option>
+                </select>
+                <button id="new-terminal">+</button>
+                <button id="split">Split</button>
+                <button id="delete">Del</button>
+                <button id="full-view">Full View</button>
+                <button id="close">X</button>
+            </div>
+        </header>
+        <section id="console"/>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'Console'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

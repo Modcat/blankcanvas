bb54596a22ff9a4b467e79b86ce4417c2d3e46de
Modcat <lawrence.turton@gmail.com>
2020-04-28 09:57:45 +0100

    working desktop config

--- desktop-blankcanvas/server/src/services/hardwareprofile/hardwareprofile.class.js
+++
@@ -1,5 +0,0 @@
-const { Service } = require('feathers-nedb');
-
-exports.Hardwareprofile = class Hardwareprofile extends Service {
-  
-};
--- desktop-blankcanvas/store/modules.js
+++
@@ -1,9 +0,0 @@
-// export const state = () => ({
-//     crypto: require ? require('crypto') : false,
-//     childProcess: require ? require('child_process') : false,
-//     os: require ? require('os') : false,
-//     fs: require ? require('fs') : false,
-//     zlib: require ? require('zlib') : false,
-//     path: require ? require('path') : false,
-//     process: require ? require('process') : false,
-// })
